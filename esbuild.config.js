af8371f78a7212787135837f220bb4da20a04d2a
Ehsan Azish <ehzish@3nsofts.com>
2025-11-21 10:06:47 +0000

    fixed dependency versions and added package and publish scripts

--- esbuild.config.js
+++ esbuild.config.js
@@ -6,7 +6,7 @@ const buildOptions = {
   entryPoints: ['src/extension.ts'],
   bundle: true,
   outfile: 'dist/extension.js',
-  external: ['vscode'],
+  external: ['vscode', 'tree-sitter', 'tree-sitter-swift'],
   format: 'cjs',
   platform: 'node',
   sourcemap: true,
